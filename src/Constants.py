9ba7e6e7872a4f378eaefdb7c8b3a8069cd48033
gaoxiaofeng <xiaofeng355@sina.com>
2018-02-02 10:51:42 +0800

    修改比例信息

--- src/Constants.py
+++ src/Constants.py
@@ -18,12 +18,13 @@ INSTRUCTIONS = '\n 1. 通过【选项】或者手动填写 [scale] [scale densit
                '\n 3. 选择【开始】' \
                '\n\n' \
                '\ndensity、scale density、xdpi 通过API获取为准，参考如下：' \
-               '\n|  等级   | 分辨率（px）| 屏幕密度（dpi）| 换算（px/dp）| 比例 |' \
-               '\n|  ldpi    |   240x320    |         120         |   1dp=0.75px  |   3   |' \
-               '\n|  mdpi  |   320x480    |         160         |   1dp=1px      |   4   |' \
-               '\n|  hdpi   |   480x800    |         240         |   1dp=1.5px    |   6   |' \
-               '\n|  xhdpi  |   720x1280  |         320         |   1dp=2px      |   8   |' \
-               '\n|  xxhdpi |  1080x1920 |         480         |   1dp=3px      |  12  |' \
+               '\n|  等级   | 分辨率（px）| 屏幕密度（dpi）| 换算（px/dp）| 图标 |' \
+               '\n|  ldpi    |   240x320    |         120         |   1dp=0.75px  |   36 * 36   |' \
+               '\n|  mdpi  |   320x480    |         160         |   1dp=1px      |   48 * 48   |' \
+               '\n|  hdpi   |   480x800    |         240         |   1dp=1.5px    |   72 * 72   |' \
+               '\n|  xhdpi  |   720x1280  |         320         |   1dp=2px      |   96 * 96   |' \
+               '\n|  xxhdpi |  1080x1920 |         480         |   1dp=3px      |  144 * 144  |' \
+               '\n|  xxhdpi |  1440x2560    |     640         |   1dp=4px      |  192 * 192  |' \
 
 
 AUTHOR = '作者：xiaofeng' \
